d7e2eade9ca607c9441f72409db588534d1d93a6
Joel Brett <brettj@wellmark.com>
2025-05-21 13:02:02 -0500

    Add demo/development page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ThreadedMessage } from '../../dist/willow';
+import { ThreadedMessagesListComponent } from '../../projects/willow/src/public-api';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ThreadedMessagesListComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '@wellmark/angular-willow';
+  threadedMessages: ThreadedMessage[] = [
+    {
+      name: 'Message 1 Name',
+      date: '02/02/2025',
+      text: 'This is the first message',
+      isRepresentative: false,
+      correspondence: '12345'
+    },
+    {
+      name: 'Message 2 Name',
+      date: '02/02/2025',
+      text: 'This is the second message',
+      isRepresentative: true,
+      correspondence: '67890'
+    }
+  ]
 }
